6459d1c4e5cb81b44bc48cf12d8b492412633596
Félix Bourbonnais <felix.bourbonnais@imaginary-spaces.com>
2020-11-02 14:24:40 -0500

    !fixup fix the python test harness

--- src/domain_tests/test_domain_reload.py
+++ src/domain_tests/test_domain_reload.py
@@ -7,16 +7,10 @@ def runit(m1, m2, member):
 
     assert proc.returncode == 0
 
-def test_remove_method():
+def test_rename_class():
 
-    m1 = 'public static void TestMethod() {Console.WriteLine("from test method");}'
-    m2 = 'public static void TestMethod2() {Console.WriteLine("from test method");}'
-    member = 'TestMethod'
+    m1 = 'TestClass'
+    m2 = 'TestClass2'
+    member = ''
     runit(m1, m2, member)
 
-def test_remove_member():
-
-    m1 = 'public static int TestMember = -1;'
-    m2 = 'public static int TestMember2 = -1;'
-    member = 'TestMember'
-    runit(m1, m2, member)
