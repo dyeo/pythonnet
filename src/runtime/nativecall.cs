0bc044a996d7e0e21c1d32a975161328a7a7a5df
Dan <dan@dyeo.net>
2021-07-08 11:55:21 -0400

    Remove unnecessary usings

--- src/runtime/nativecall.cs
+++ src/runtime/nativecall.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Reflection;
-using System.Reflection.Emit;
 using System.Runtime.InteropServices;
-using System.Threading;
 
 namespace Python.Runtime
 {
--- src/runtime/runtime.cs
+++ src/runtime/runtime.cs
@@ -1,8 +1,6 @@
-using System.Reflection.Emit;
 using System;
 using System.Diagnostics.Contracts;
 using System.Runtime.InteropServices;
-using System.Security;
 using System.Text;
 using System.Threading;
 using System.Collections.Generic;
